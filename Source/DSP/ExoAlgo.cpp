b6f558aed0ef99caa96a02cdc4406c69fad8852d
ivanpollak <ivanpollak@tutanota.com>
2025-01-09 13:49:43 +0100

    👷‍♂️ Updated algorythm so it can be used in a processorChain

--- Source/DSP/ExoAlgo.cpp
+++ Source/DSP/ExoAlgo.cpp
@@ -3,7 +3,8 @@
 #include "ExoAlgo.h"
 #include <cmath>
 
-float Algorythm::normalizeBetweenThresholds(float input, float softenTreshold, float hardTreshold) {
+template <typename SampleType>
+float ExoAlgo<SampleType>::normalizeBetweenThresholds(float input, float softenTreshold, float hardTreshold) {
     float tresholdDifference;
     float adjustedInput;
     float output;
@@ -15,7 +16,8 @@ float Algorythm::normalizeBetweenThresholds(float input, float softenTreshold, f
     return output;
 };
 
-float Algorythm::applySinusoidalClip(float inputValue, float scalingFactor, float maxThreshold)
+template <typename SampleType>
+float ExoAlgo<SampleType>::applySinusoidalClip(float inputValue, float scalingFactor, float maxThreshold)
 {
     float finalOutput;
     bool isInputNegative = inputValue < 0;
@@ -50,4 +52,40 @@ float Algorythm::applySinusoidalClip(float inputValue, float scalingFactor, floa
     }
 
     return finalOutput;
+};
+
+template <typename SampleType>
+void ExoAlgo<SampleType>::prepare(const juce::dsp::ProcessSpec& spec) noexcept
+{
+    sampleRate = static_cast<SampleType>(spec.sampleRate);
+};
+
+template <typename SampleType>
+void ExoAlgo<SampleType>::reset() 
+{
+
+};
+
+template <typename SampleType>
+template <typename ProcessContext>
+void ExoAlgo<SampleType>::process(const ProcessContext& context)
+{
+    auto& inputBlock = context.getInputBlock();
+    auto& outputBlock = context.getOutputBlock();
+
+    // Ensure the input and output sizes match
+    jassert(inputBlock.getNumSamples() == outputBlock.getNumSamples());
+    jassert(inputBlock.getNumChannels() == outputBlock.getNumChannels());
+
+    for (size_t channel = 0; channel < inputBlock.getNumChannels(); ++channel)
+    {
+        auto* input = inputBlock.getChannelPointer(channel);
+        auto* output = outputBlock.getChannelPointer(channel);
+
+        for (size_t sample = 0; sample < inputBlock.getNumSamples(); ++sample)
+        {
+            // Example: Apply the sinusoidal clipping algorithm
+            output[sample] = applySinusoidalClip(input[sample], scalingFactor, maxThreshold);
+        }
+    }
 }
--- Source/DSP/ExoAlgo.h
+++ Source/DSP/ExoAlgo.h
@@ -1,9 +1,19 @@
 #ifndef EXOALGO_H
 #define EXOALGO_H
-class Algorythm
+
+#include <JuceHeader.h>
+
+template <typename SampleType> class ExoAlgo
 {
 public:
 	float normalizeBetweenThresholds(float rawInput, float softenTreshold, float hardTreshold);
 	float applySinusoidalClip(float inputValue, float scaleFactor, float maxThreshold);
+
+	void prepare(const juce::dsp::ProcessSpec& spec) noexcept;
+	void reset();
+	template <typename ProcessContext> void process(const ProcessContext& context);
+
+private:
+	SampleType sampleRate = 44100.0; // Default sample rate
 };
 #endif
