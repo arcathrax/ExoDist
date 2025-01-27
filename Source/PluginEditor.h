123a8d4142e7bc03847848c57d042f110c71afc2
Ivan <arcathrax@proton.me>
2025-01-27 15:56:04 +0100

    👷‍♂️ updated `CustomRotarySlider` struct

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -19,10 +19,37 @@ struct CustomRotarySlider : juce::Slider
         juce::Slider::TextEntryBoxPosition::NoTextBox
     )
     {
-        
     }
-    
+
+    // Überschreiben der paint-Methode
+    void paint (juce::Graphics& g) override
+    {
+        const float diameter = juce::jmin(getWidth(), getHeight());
+        const float radius = diameter / 2.0f;
+        const float centerX = getWidth() / 2.0f;
+        const float centerY = getHeight() / 2.0f;
+
+        g.setColour(juce::Colour(0xFF1a1a19));
+        g.fillEllipse(centerX - radius, centerY - radius, diameter, diameter);
+
+        float value = getValue();
+        float minValue = getMinimum();
+        float maxValue = getMaximum();
+
+        float normalizedValue = (value - minValue) / (maxValue - minValue);
+
+        float angle = (normalizedValue*5)-(juce::MathConstants<float>::pi*1.25);
+
+        const float endX = centerX + radius * std::cos(angle);
+        const float endY = centerY + radius * std::sin(angle);
+
+        g.setColour(juce::Colour(0xFF4f8131));
+        g.drawLine(centerX, centerY, endX, endY, getWidth()*0.03);
+    }
 };
+
+
+
 
 //==============================================================================
 /**
@@ -77,10 +104,12 @@ private:
 
     std::vector<juce::Component*> getComps();
     
-    juce::uint32 BackgroundColor = 0xFF1a1a19;
-    juce::uint32 ContentColor = 0xFF4f8131;
-    juce::uint32 TitleColor = 0xFF80993c;
-    juce::uint32 HighlightColor = 0xFFf6fcdf;
+    juce::uint32 BackgroundColor = 0xFFf6fcdf;
+    juce::uint32 ContentColor = 0xFF80993c;
+    juce::uint32 TitleColor = 0xFF4f8131;
+    juce::uint32 HighlightColor = 0xFF1a1a19;
+    
+    
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ExoDistAudioProcessorEditor)
